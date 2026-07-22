662917fbc209d17ad347f9c6c2b623cbff891906
NotAHuman1230 <mendozajerichojohn@outlook.com>
2026-07-22 22:39:42 +0100

    Feature: swinging + other changes

--- gmtk-2026/abilities/conjure_weapon/conjure_weapon.gd
+++ gmtk-2026/abilities/conjure_weapon/conjure_weapon.gd
@@ -4,29 +4,19 @@ extends Ability
 # [TODO] Wait till we get proper controls in project settings
 # [TODO] Have cost when timer is setup
 
-signal on_weapon_generated
 signal on_weapon_thrown
 
 var _player: Node2D 
 var _weapon_data: WeaponData
 
-var _has_weapon: bool = false
 var _can_attack: bool = true
 
 func activate_ability() -> void:
-	if _has_weapon:
-		_throw()
-	else:
-		_generate_weapon()
+	_throw()
 
 func process(_delta: float) -> void:
-	if _has_weapon and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and _can_attack:
-		_can_attack = false
-		
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and _can_attack:
 		_attack()
-		
-		await _player.get_tree().create_timer(_weapon_data.attack_delay).timeout
-		_can_attack = true
 
 func _init(player: Node2D, weapon_data: WeaponData) -> void:
 	_player = player
@@ -34,11 +24,17 @@ func _init(player: Node2D, weapon_data: WeaponData) -> void:
 	_ability_data = preload("res://abilities/conjure_weapon/conjure_data.tres")
 
 func _attack() -> void:
-	print("attack")
+	_can_attack = false
+	
+	var instance: SwingableWeapon = _weapon_data.swingable.instantiate()
+	instance.set_player(_player)
+	_player.get_tree().current_scene.add_child(instance)
+	await instance.on_finished
+	
+	_can_attack = true
 
 func _throw() -> void:
 	on_weapon_thrown.emit()
-	_has_weapon = false
 	
 	var instance: ThrowableWeapon = _weapon_data.throwable.instantiate()
 	_player.get_tree().current_scene.add_child(instance)
@@ -46,10 +42,3 @@ func _throw() -> void:
 	
 	var direction: Vector2 = (_player.get_global_mouse_position() - _player.global_position).normalized()
 	instance.set_direction(direction)
-
-func _generate_weapon() -> void:
-	print("generating")
-	_has_weapon = true
-	_can_attack = true
-	
-	on_weapon_generated.emit()
--- gmtk-2026/abilities/conjure_weapon/weapons_data/weapon_data.gd
+++ gmtk-2026/abilities/conjure_weapon/weapons_data/weapon_data.gd
@@ -3,11 +3,5 @@ extends Resource
 
 # [NOTE] damage type deponds on Suriya on how he does health
 
-@export var name: String
-
-@export_group("Holding")
-@export var hold_damage: int = 1
-@export var attack_delay: float = 0.5
-
-@export_group("Throwing")
+@export var swingable: PackedScene
 @export var throwable: PackedScene

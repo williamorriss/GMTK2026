4155e91db2284162e553889016543451b38aa1d8
Suriya Velayudham <suriyavelayudham@gmail.com>
2026-07-23 14:29:04 +0100

    Using navigation region instead

--- gmtk-2026/enemies/melee enemies/grunt.gd
+++ gmtk-2026/enemies/melee enemies/grunt.gd
@@ -3,28 +3,26 @@ extends CharacterBody2D
 
 @export var speed = 50.0
 @export var atk = 10.0
-@onready var parent = self.get_parent()
-@onready var player = self.get_parent().get_child(0)
-
-var index = 0
+@export var player: Node2D
 
 
+
+func _ready():
+	add_to_group("enemies")
+	
 func _physics_process(delta: float) -> void:
-	var path = parent.return_path()
-	if path.is_empty() or index >= path.size():
-		velocity = Vector2i.ZERO
-		move_and_slide()
-		return
 		
-	var target = path[index]
+	var target = player.global_position
 	var direction = (target-global_position).normalized()
 	velocity = direction * speed
 
 	move_and_slide()
 	
-	if global_position.distance_to(target) <= 4.0:
-		index += 1
 
 func attack():
 	if global_position.distance_to(player.global_position) <= 4.0:
 		player.health -= atk
+		var temp_velocity = velocity
+		velocity = velocity * -0.1
+		move_and_slide()
+		velocity = temp_velocity
--- gmtk-2026/navigation.gd
+++ gmtk-2026/navigation.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends NavigationRegion2D
 
 
 # Called when the node enters the scene tree for the first time.

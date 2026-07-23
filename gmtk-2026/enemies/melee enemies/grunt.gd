2b503cde9a9e670d117cbc770e9fcd05469d0091
NotAHuman1230 <mendozajerichojohn@outlook.com>
2026-07-23 14:48:53 +0100

    Feature: enemies

--- gmtk-2026/enemies/melee enemies/grunt.gd
+++ gmtk-2026/enemies/melee enemies/grunt.gd
@@ -1,28 +1,29 @@
 extends CharacterBody2D
 
+@export var agent: NavigationAgent2D
 
-@export var speed = 50.0
-@export var atk = 10.0
+@export var speed: float = 50.0
+@export var atk: float = 10.0
 @export var player: Node2D
 
-
-
-func _ready():
+func _ready() -> void:
 	add_to_group("enemies")
-	
-func _physics_process(delta: float) -> void:
-		
-	var target = player.global_position
-	var direction = (target-global_position).normalized()
-	velocity = direction * speed
 
+func _physics_process(_delta: float) -> void:
+	if agent.is_navigation_finished():
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+	
+	agent.target_position = player.position
+	
+	var next_point: Vector2 = agent.get_next_path_position()
+	var direction: Vector2 = global_position.direction_to(next_point)
+	
+	velocity = direction * speed
 	move_and_slide()
 	
 
-func attack():
-	if global_position.distance_to(player.global_position) <= 4.0:
-		player.health -= atk
-		var temp_velocity = velocity
-		velocity = velocity * -0.1
-		move_and_slide()
-		velocity = temp_velocity
+func attack() -> void:
+	pass
+	#will do later

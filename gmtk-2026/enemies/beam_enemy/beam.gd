57f3c40cad85be1fddd80b80d7825b14fd7270ca
NotAHuman1230 <mendozajerichojohn@outlook.com>
2026-07-26 11:49:12 +0100

    fjadios

--- gmtk-2026/enemies/beam_enemy/beam.gd
+++ gmtk-2026/enemies/beam_enemy/beam.gd
@@ -17,35 +17,61 @@ signal on_finish_charge
 signal on_ready
 signal on_finish_beam
 
-static func create_beam(caster: Enemy, direction: Vector2, pos: Vector2) -> Beam:
+var _current_state: State = State.Idle
+enum State {
+	Idle,
+	Charging,
+	Firing,
+	Fired
+}
+
+static func create_beam(caster_owner: Enemy, direction: Vector2, pos: Vector2) -> Beam:
 	var instance: Beam = preload("res://enemies/beam_enemy/beam.tscn").instantiate()
-	instance.caster = caster
+	instance.caster = caster_owner
 	instance.rotation = direction.angle()
 	instance.position = pos
 	instance.on_ready.emit()
+	instance.visible = false
 	return instance
-
-func _ready() -> void:
-	animator.speed_scale = 1 / charging_time
-	animator.play("charging")
-	animator.animation_finished.connect(func (_x: Variant) -> void: _draw_beam(), CONNECT_ONE_SHOT)
+	
+func activate(charging_duration: float) -> void:
+	_animate_duration("charging", charging_duration)
+	_current_state = State.Charging
 	await animator.animation_finished
-	on_finish_charge.emit()
+	
+func fire(lifetime: float) -> void:
+	_current_state = State.Firing
 	await get_tree().create_timer(lifetime).timeout
-	on_finish_beam.emit()
+	_destroy()
+	
+func _destroy() -> void:
 	queue_free()
 	
-func _process(delta: float) -> void:
-	if not animator.animation_finished:
-		return
+func _physics_process(delta: float) -> void:
+	if _current_state == State.Firing or _current_state == State.Fired:
+		_dmg_timer = max(0.0, _dmg_timer - delta)
+		if _dmg_timer <= 0.0:
+			_hit()
+			_dmg_timer = damage_timer
 		
-	_dmg_timer = max(0.0, _dmg_timer - delta)
-	if _dmg_timer <= 0.0:
-		_hit()
-		_dmg_timer = damage_timer
+func _process(delta: float) -> void:
+	if _current_state == State.Charging:
+		_target_player()
+		_draw_follow_beam()
+	elif _current_state == State.Firing:
+		_draw_beam()
+		_target_player()
+		_current_state = State.Fired
+
+func _target_player() -> void:
+	var player: Player = get_player()
+	if not player:
+		return
+	
+	look_at(player.global_position)
 
 func _hit() -> void:
-	var space_state = get_world_2d().direct_space_state
+	var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
 	
 	var ray: Vector2 = Vector2.RIGHT.rotated(global_rotation).normalized()
 	var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(
@@ -59,15 +85,72 @@ func _hit() -> void:
 	if result.is_empty():
 		return
 		
-	var health = Health.get_health(result.collider)
-	health.damage(damage, global_position.direction_to(result.position), Health.Owner.Enemy)
+	var health: Health = Health.get_health(result.collider)
+	if health:
+		print("hit")
+		health.damage(damage, global_position.direction_to(result.position), Health.Owner.Enemy)
 
+
+func get_player() -> Player:
+	var player_array = get_tree().get_nodes_in_group("player")
+	if player_array.size() < 1:
+		_fail()
+		return null
+	return player_array[0]
+		
+
+
+
+func _draw_follow_beam() -> void:
+	visible = true
+	var player: Player = get_player()
+	if not player:
+		return
+		
+	var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+	var ray: Vector2 = Vector2.RIGHT.rotated(global_rotation).normalized()
+	var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(
+		global_position,
+		player.global_position,
+		1 | 4
+	)
+	query.exclude = [self]
+	var result: Dictionary = space_state.intersect_ray(query)
+	if result.is_empty():
+		return
+		
+	_local_draw_line(result, ray)
+	
+	
+func _local_draw_line(result: Dictionary, ray: Vector2) -> void:
+	if not caster:
+		return
+	
+	var end_local: Vector2
+	if result.is_empty():
+		end_local = ray * max_length
+	else:
+		end_local = to_local(result.position)
+		
+	points = PackedVector2Array([
+		to_local(caster.global_position),
+		end_local
+	])
+	
+	_line_end.global_position = to_global(end_local)
+	_line_end.global_rotation = global_rotation
+	
 	
 func _draw_beam() -> void:
-	var space_state = get_world_2d().direct_space_state
+	var player: Player = get_player()
+	if not player:
+		return
+		
+	var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
 	
 	# use global coordinates, not local to node
-	var ray: Vector2 = Vector2.RIGHT.rotated(global_rotation).normalized()
+	
+	var ray: Vector2 = global_position.direction_to(player.global_position).normalized()
 	var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(
 		global_position,
 		global_position + ray * max_length,
@@ -75,18 +158,15 @@ func _draw_beam() -> void:
 	)
 	query.exclude = [self]
 	var result: Dictionary = space_state.intersect_ray(query)
-
-	var end_local: Vector2
-	if result.is_empty():
-		end_local = ray * max_length  # local space, since ray is already computed off global_rotation but positions are relative to self
-	else:
-		end_local = to_local(result.position)
-		
-	points = PackedVector2Array([
-		Vector2(0,0),
-		end_local
-	])
+	_local_draw_line(result, ray)
 	
-	_line_end.visible = true
-	_line_end.global_position = to_global(end_local)
-	_line_end.global_rotation = global_rotation + PI
+	
+func _fail() -> void:
+	_destroy()
+	
+func _animate_duration(anim_name: String, target_duration: float) -> void:
+	animator.speed_scale = 1 / target_duration
+	animator.play(anim_name)	
+	
+
+	
--- gmtk-2026/enemies/beam_enemy/beam_enemy.gd
+++ gmtk-2026/enemies/beam_enemy/beam_enemy.gd
@@ -18,7 +18,6 @@ extends Enemy
 @onready var _animation: AnimatedSprite2D = $AnimatedSprite2D
 
 var _offset: Vector2 = Vector2.ZERO
-var _can_attack: bool = true
 var _shooting: bool = false
 
 func _ready() -> void:
@@ -44,7 +43,11 @@ func _move(delta: float) -> void:
 	if not _closest_player:
 		calc_closest_player()
 		return
-
+		
+	if _shooting:
+		return
+	
+	_animation.speed_scale = 1.0
 	_animation.play("walk")
 	agent.target_position = _closest_player.global_position + _offset
 
@@ -57,26 +60,27 @@ func _move(delta: float) -> void:
 	agent.set_velocity(direction * speed)
 
 func _attack() -> void:
-	if not _can_attack or not _closest_player:
+	if _shooting or not _closest_player:
 		return
 
-	_can_attack = false
 	_shooting = true
 
 	var dir: Vector2 = global_position.direction_to(_closest_player.global_position)
 	var pos: Vector2 = global_position + dir * beam_offset
 
 	var beam: Beam = Beam.create_beam(self, dir, pos)
-	var charge_time: float = beam.charging_time
-
 	get_tree().current_scene.add_child(beam)
-	beam.on_finish_charge.connect(func () -> void: _animate_duration("attack", beam.damage_timer))
-	_animate_duration("charging", beam.charging_time)
 	
-	get_tree().create_timer(attack_cooldown).timeout.connect(func () -> void: _can_attack = true)
-
-	beam.on_finish_beam.connect(func () -> void: _shooting = false)
-
+	beam.activate.call_deferred(charging_time)
+	_animate_duration("charging", charging_time)
+	await _animation.animation_finished
+	print("finished charging")
+	beam.fire.call_deferred(beam_lifetime)
+	_animate_duration("attack", beam_lifetime)
+	await _animation.animation_finished
+	print("finished attacking")
+	await get_tree().create_timer(attack_cooldown).timeout
+	_shooting = false
 
 func _animate_duration(anim_name: String, target_duration: float) -> void:
 	var sf: SpriteFrames = _animation.sprite_frames
@@ -86,7 +90,9 @@ func _animate_duration(anim_name: String, target_duration: float) -> void:
 	_animation.play(anim_name)
 
 func _on_dead(dealer: Health.Owner, taker: Health.Owner, direction: Vector2) -> void:
+	_animation.speed_scale = 1.0
 	_animation.play("death")
+	await _animation.animation_finished
 	queue_free()
 
 

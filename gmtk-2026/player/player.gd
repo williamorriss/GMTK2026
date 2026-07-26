0ba1b5b10893a0cbab999f08cc1bdda5dbd6aea0
NotAHuman1230 <mendozajerichojohn@outlook.com>
2026-07-26 17:11:14 +0100

    fric

--- gmtk-2026/player/player.gd
+++ gmtk-2026/player/player.gd
@@ -8,7 +8,7 @@ extends CharacterBody2D
 @export_group("Movement")
 @export var speed: float = 300.0
 @export var acceleration: float = 2000.0
-@export var friction: float = 2000.0
+@export var friction: float = 4000.0
 
 @export_group("Dashing")
 @export var dash_speed: float = 900.0

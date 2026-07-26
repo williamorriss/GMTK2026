cfc460df8ef12c444dd4590e1c9f46ab03f4d9ea
NotAHuman1230 <mendozajerichojohn@outlook.com>
2026-07-26 02:39:28 +0100

    fdajksljio

--- gmtk-2026/jericho_level/level_end/level_end.gd
+++ gmtk-2026/jericho_level/level_end/level_end.gd
@@ -1,12 +1,11 @@
 extends Area2D
 
 @export_file("*.tscn") var next_scene: String
-@export_file("*.tscn") var next_level: String
 
 func _ready() -> void:
 	var _x: bool = body_entered.connect(_entered)
 
 func _entered(body: Node2D) -> void:
 	if body is Player:
-		StateHolder.set_next_level(next_level)
-		await SceneTransition.change_scene(next_scene)
+		StateHolder.set_next_level(next_scene)
+		await SceneTransition.change_scene("res://UI/next_level/next_scene.tscn")

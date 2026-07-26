962a80366988ce322321f900272fa349f27ac130
maurice-borys <brillbusmorriss@gmail.com>
2026-07-26 15:41:00 +0100

    maybe?

--- gmtk-2026/enemies/beam_enemy/beam_enemy.gd
+++ gmtk-2026/enemies/beam_enemy/beam_enemy.gd
@@ -17,85 +17,85 @@ extends Enemy
 
 @onready var _animation: AnimatedSprite2D = $AnimatedSprite2D
 
-var _offset: Vector2 = Vector2.ZERO
-var _shooting: bool = false
-
-func _ready() -> void:
-	super._ready()
-	
-	add_to_group("enemies")
-	var _x: int = health.on_dead.connect(_on_dead)
-	
-	var rot: float = randf_range(0, 2 * PI)
-	_offset = Vector2(cos(rot), sin(rot)) * offset_distance
-
-func _physics_process(delta: float) -> void:
-	if not _closest_player:
-		calc_closest_player()
-		return
-		
-	if global_position.distance_to(_closest_player.global_position) < attack_range:
-		await _attack()
-	else:
-		_move(delta)
-
-func _move(delta: float) -> void:
-	if not _closest_player:
-		calc_closest_player()
-		return
-		
-	if _shooting:
-		return
-	
-	_animation.speed_scale = 1.0
-	_animation.play("walk")
-	agent.target_position = _closest_player.global_position + _offset
-
-	if agent.is_navigation_finished() or global_position.distance_to(_closest_player.global_position) < max_distance or _shooting:
-		agent.set_velocity(Vector2.ZERO)
-		return
-
-	var next_point: Vector2 = agent.get_next_path_position()
-	var direction: Vector2 = global_position.direction_to(next_point)
-	agent.set_velocity(direction * speed)
-
-func _attack() -> void:
-	if _shooting or not _closest_player:
-		return
-
-	_shooting = true
-
-	var dir: Vector2 = global_position.direction_to(_closest_player.global_position)
-	var pos: Vector2 = global_position + dir * beam_offset
-
-	var beam: Beam = Beam.create_beam(self, dir, pos)
-	get_tree().current_scene.add_child(beam)
-	
-	beam.activate.call_deferred(charging_time)
-	_animate_duration("charging", charging_time)
-	await _animation.animation_finished
-	print("finished charging")
-	beam.fire.call_deferred(beam_lifetime)
-	_animate_duration("attack", beam_lifetime)
-	await _animation.animation_finished
-	print("finished attacking")
-	await get_tree().create_timer(attack_cooldown).timeout
-	_shooting = false
-
-func _animate_duration(anim_name: String, target_duration: float) -> void:
-	var sf: SpriteFrames = _animation.sprite_frames
-	var frame_count: int = sf.get_frame_count(anim_name)
-	var target_fps: float = frame_count / target_duration
-	_animation.speed_scale = target_fps / sf.get_animation_speed(anim_name)
-	_animation.play(anim_name)
-
-func _on_dead(dealer: Health.Owner, taker: Health.Owner, direction: Vector2) -> void:
-	_animation.speed_scale = 1.0
-	_animation.play("death")
-	await _animation.animation_finished
-	queue_free()
-
-
-func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
-	velocity = safe_velocity
-	var _z: int = move_and_slide()
+#var _offset: Vector2 = Vector2.ZERO
+#var _shooting: bool = false
+#
+#func _ready() -> void:
+	#super._ready()
+	#
+	#add_to_group("enemies")
+	#var _x: int = health.on_dead.connect(_on_dead)
+	#
+	#var rot: float = randf_range(0, 2 * PI)
+	#_offset = Vector2(cos(rot), sin(rot)) * offset_distance
+#
+#func _physics_process(delta: float) -> void:
+	#if not _closest_player:
+		#calc_closest_player()
+		#return
+		#
+	#if global_position.distance_to(_closest_player.global_position) < attack_range:
+		#await _attack()
+	#else:
+		#_move(delta)
+#
+#func _move(delta: float) -> void:
+	#if not _closest_player:
+		#calc_closest_player()
+		#return
+		#
+	#if _shooting:
+		#return
+	#
+	#_animation.speed_scale = 1.0
+	#_animation.play("walk")
+	#agent.target_position = _closest_player.global_position + _offset
+#
+	#if agent.is_navigation_finished() or global_position.distance_to(_closest_player.global_position) < max_distance or _shooting:
+		#agent.set_velocity(Vector2.ZERO)
+		#return
+#
+	#var next_point: Vector2 = agent.get_next_path_position()
+	#var direction: Vector2 = global_position.direction_to(next_point)
+	#agent.set_velocity(direction * speed)
+#
+##func _attack() -> void:
+	##if _shooting or not _closest_player:
+		##return
+##
+	##_shooting = true
+##
+	##var dir: Vector2 = global_position.direction_to(_closest_player.global_position)
+	##var pos: Vector2 = global_position + dir * beam_offset
+##
+	##var beam: Beam = Beam.create_beam(self, dir, pos)
+	##get_tree().current_scene.add_child(beam)
+	##
+	##beam.activate.call_deferred(charging_time)
+	##_animate_duration("charging", charging_time)
+	##await _animation.animation_finished
+	##print("finished charging")
+	##beam.fire.call_deferred(beam_lifetime)
+	##_animate_duration("attack", beam_lifetime)
+	##await _animation.animation_finished
+	##print("finished attacking")
+	##await get_tree().create_timer(attack_cooldown).timeout
+	##_shooting = false
+#
+#func _animate_duration(anim_name: String, target_duration: float) -> void:
+	#var sf: SpriteFrames = _animation.sprite_frames
+	#var frame_count: int = sf.get_frame_count(anim_name)
+	#var target_fps: float = frame_count / target_duration
+	#_animation.speed_scale = target_fps / sf.get_animation_speed(anim_name)
+	#_animation.play(anim_name)
+#
+#func _on_dead(dealer: Health.Owner, taker: Health.Owner, direction: Vector2) -> void:
+	#_animation.speed_scale = 1.0
+	#_animation.play("death")
+	#await _animation.animation_finished
+	#queue_free()
+#
+#
+#func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	#velocity = safe_velocity
+	#var _z: int = move_and_slide()
